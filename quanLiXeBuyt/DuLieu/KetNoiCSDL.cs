1feba74398d7daff24671ff2cb085347a2d6a234
DTH235740_NGUYENTHIYENPHUONG <phuong_dth235740@student.agu.edu.vn>
2026-02-03 10:26:07 +0700

    up

---
+++ quanLiXeBuyt/DuLieu/KetNoiCSDL.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Data;
+using MySql.Data.MySqlClient;
+
+namespace quanLiXeBuyt.DuLieu
+{
+    public class KetNoiCSDL
+    {
+        // Thay mật khẩu MySQL của bạn vào đây
+        private string chuoiKetNoi = "Server=localhost;Database=QuanLyXeBuyt;Uid=root;Pwd=123456;Charset=utf8mb4";
+
+        public MySqlConnection LayKetNoi()
+        {
+            return new MySqlConnection(chuoiKetNoi);
+        }
+
+        // Hàm lấy dữ liệu (đổ vào DataGridView, ComboBox...)
+        public DataTable LayDuLieu(string sql)
+        {
+            DataTable dt = new DataTable();
+            using (MySqlConnection conn = LayKetNoi())
+            {
+                MySqlDataAdapter da = new MySqlDataAdapter(sql, conn);
+                da.Fill(dt);
+            }
+            return dt;
+        }
+
+        // Hàm thực thi (Thêm, Xóa, Sửa)
+        public int ThucThi(string sql, MySqlParameter[] thamSo = null)
+        {
+            using (MySqlConnection conn = LayKetNoi())
+            {
+                conn.Open();
+                MySqlCommand cmd = new MySqlCommand(sql, conn);
+                if (thamSo != null) cmd.Parameters.AddRange(thamSo);
+                return cmd.ExecuteNonQuery();
+            }
+        }
+    }
+}
